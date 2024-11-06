82d3b2c6f9f81b539b403958a9a307654460ee6e
Angelo Melonas <angelomelonas@gmail.com>
2024-11-06 22:18:32 +0100

    Fix input arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,13 @@ import HTTPServer.HTTPServer;
 public class Main {
 
     public static void main(String[] args) {
-        String directory = "";
-        if (args[0] != null) {
-            directory = args[0];
+        String directory = null;
+
+        for (int i = 0; i < args.length; i++) {
+            if ("--directory".equals(args[i]) && i + 1 < args.length) {
+                directory = args[i + 1];
+                break;
+            }
         }
 
         HTTPServer httpServer = new HTTPServer(4221, directory);
