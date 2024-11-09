9fe892dfe2bf6c99f8d13ffa3327bdb33ff9bad8
Angelo Melonas <angelomelonas@gmail.com>
2024-11-09 16:30:51 +0100

    Support multiple encodings

--- src/main/java/Middleware/CompressionMiddleware.java
+++ src/main/java/Middleware/CompressionMiddleware.java
@@ -4,6 +4,8 @@ import Enum.Header;
 import Request.Request;
 import Response.Response;
 
+import java.util.Arrays;
+
 public class CompressionMiddleware implements Middleware {
     @Override
     public void handleRequest(Request request) {
@@ -13,9 +15,11 @@ public class CompressionMiddleware implements Middleware {
     @Override
     public void handleResponse(Request request, Response response) {
         String acceptEncoding = request.getHeaderParameter(Header.ACCEPT_ENCODING);
-
-        if (acceptEncoding != null && acceptEncoding.equals("gzip")) {
-            response.setHeader(Header.CONTENT_ENCODING, "gzip");
+        if (acceptEncoding != null) {
+            String[] acceptEncodings = acceptEncoding.split(", ");
+            if (Arrays.stream(acceptEncodings).anyMatch("gzip"::equalsIgnoreCase)) {
+                response.setHeader(Header.CONTENT_ENCODING, "gzip");
+            }
         }
     }
 }
